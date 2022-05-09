26d2b639161ff7bfbb3878dc3f5f627ceb147514
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-05-09 09:24:26 +0100

    add login fonctionality

--- app/src/main/java/com/firebasedemo/MainActivity.java
+++ app/src/main/java/com/firebasedemo/MainActivity.java
@@ -2,13 +2,35 @@ package com.firebasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private final FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.logoutBtn).setOnClickListener(v -> {
+            auth.signOut();
+            startLoginActivity();
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if( auth.getCurrentUser() == null)
+            startLoginActivity();
+    }
+
+    public void startLoginActivity(){
+        finish();
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
